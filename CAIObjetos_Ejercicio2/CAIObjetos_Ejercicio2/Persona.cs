d1f55f1e2a35d033781b5dc795b91fadb4ab5001
michellerosenblat <70615029+michellerosenblat@users.noreply.github.com>
2020-09-07 00:38:32 -0300

    cai obj

---
+++ CAIObjetos_Ejercicio2/CAIObjetos_Ejercicio2/Persona.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIObjetos_Ejercicio2
+{
+    class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime fechanacimiento;
+        private int ubicacion;
+        List<string> perros = new List<string>();
+        int edad;
+
+        internal string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        internal string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+            set
+            {
+                apellido = value;
+            }
+        }
+        internal int Ubicacion
+        {
+            get
+            {
+                return ubicacion;
+            }
+        }
+        internal DateTime FechaNacimiento
+        {
+            get
+            {
+                return fechanacimiento;
+            }
+            set
+            {
+                fechanacimiento = value;
+            }
+        }
+
+        public int GetEdad()
+        {
+            
+            return (DateTime.Now.Subtract(fechanacimiento).Days)/365;
+        }
+        public void Caminar (int distancia)
+        {
+            ubicacion = ubicacion + distancia;
+        }
+        public void Adoptar (string nombreperro)
+        {
+            perros.Add(nombreperro);
+        }
+
+        public int CantidadPerros()
+        {
+            return perros.Count();
+        }
+
+        public Persona(string nom, string ape, DateTime fechanac)
+        {
+            nombre = nom;
+            apellido = ape;
+            fechanacimiento = fechanac;
+            ubicacion = 0;
+        }
+        public int Felicidad()
+        {
+            return 100 - GetEdad() + 5 * CantidadPerros();
+        }
+    }
+}
---
+++ CAIObjetos_Ejercicio2/CAIObjetos_Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIObjetos_Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechanac = new DateTime(1998, 03, 04);
+            Persona p1 = new Persona("Michelle", "Rosenblat", fechanac);
+            p1.Caminar(3);
+            Console.WriteLine ("La edad es " + p1.GetEdad());
+            Console.WriteLine("La ubicacion es " + p1.Ubicacion);
+            p1.Adoptar("Rocco");
+            p1.Adoptar("Peque");
+            p1.Adoptar("Negro");
+            Console.WriteLine("Tenes " + p1.CantidadPerros() + " perros");
+            Console.WriteLine("Tu felicidad es " + p1.Felicidad());
+            
+            Console.ReadLine();
+     
+
+
+
+        }
+    }
+}
